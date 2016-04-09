264f5e8063a142742c5b625ac8faa465b9d39be2
lvitale <luisalejovitale@gmail.com>
2016-04-08 23:06:39 -0300

    More changes

---
+++ boutique-bicicleta-servicio/entidad/Permiso.vb
@@ -0,0 +1,14 @@
+﻿Public Class Permiso
+    Inherits PermisoBase
+
+    Public Overrides Function tieneHijos() As Boolean
+        Return False
+    End Function
+
+    Public Overrides Function verificar(_codigo As String) As Boolean
+        If (codigo.Equals(codigo)) Then
+            Return True
+        End If
+        Return False
+    End Function
+End Class
--- boutique-bicicleta-utils/StringUtils.vb
+++ boutique-bicicleta-utils/StringUtils.vb
@@ -1,3 +1,23 @@
 ﻿Public Class StringUtils
 
+    Public Shared Function isNullOrEmpty(value As String) As Boolean
+        Dim nullOrEmpty As Boolean = False
+        If isNull(value) Then
+            nullOrEmpty = True
+            If value = String.Empty Then
+                nullOrEmpty = True
+            End If
+        End If
+
+        Return nullOrEmpty
+    End Function
+
+    Public Shared Function isNull(value As String) As Boolean
+        Dim isnullable As Boolean = False
+        If value Is Nothing Then
+            isnullable = True
+
+        End If
+        Return isnullable
+    End Function
 End Class
